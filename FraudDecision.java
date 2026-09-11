7667b4ca166a551e25244b98d9d67533fe8b589d
Abia Ali <aabia013@gmail.com>
2026-09-11 02:20:33 -0400

    Remove obsolete root Java duplicates

--- FraudDecision.java
+++
@@ -1,7 +0,0 @@
-package com.abia.fraudauth.fraud;
-
-public record FraudDecision(
-    int score,
-    boolean flagged,
-    String reason
-) {}
--- Role.java
+++
@@ -1,6 +0,0 @@
-package com.abia.fraudauth.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
